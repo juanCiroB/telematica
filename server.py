9eaa1a98cde46272c7a59056a8a1b701ce228476
juanCiroB <ciro87939@gmail.com>
2025-05-02 08:38:39 -0500

    server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Bienvenido al curso de Telemática'
+
+@app.route('/render')
+def render():
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+    app.run (host="0.0.0.0", port=8090)
