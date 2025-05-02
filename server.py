3295e8a8fb718ab15fd1a5d58ddbcbec2bb04564
juanCiroB <ciro87939@gmail.com>
2025-05-02 10:12:29 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bienvenido al curso de Telemática - NUEVO nuevo'
+    return 'Bienvenido al curso de Telemática - NUEVO nuevo f'
 
 @app.route('/render')
 def render():
